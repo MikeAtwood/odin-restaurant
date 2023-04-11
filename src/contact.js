00dcd0e0c6fcb635595e5d99c7bed6626b83bb32
Deadpool1 <atwood169@yahoo.com>
2023-04-10 20:30:59 -0500

    address contact info

--- src/contact.js
+++ src/contact.js
@@ -2,10 +2,18 @@ function contactPage() {
     const contact = document.createElement("div")
     contact.classList.add("contact")
 
+    const phone = document.createElement("p")
+    phone.textContent = "(123) 456 6789"
+
+    const address = document.createElement("p")
+    address.textContent = "1234 Street Dr. Minneapolis MN, 55401"
+
     const contactImg = document.createElement("img")
     contactImg.src = "../src/img/pizza-building.jpeg"
     contactImg.alt = "location"
 
+    contact.appendChild(phone)
+    contact.appendChild(address)
     contact.appendChild(contactImg);
 
     return contact;
--- src/index.js
+++ src/index.js
@@ -6,83 +6,85 @@ import loadContact from './contact';
 
 // creates header element, appends to content
 function Header() {
-const header = document.createElement('header');
-header.classList.add("header")
+  const header = document.createElement('header');
+  header.classList.add("header")
 
-const restaurantName = document.createElement("h1")
-restaurantName.classList.add("restaurant-name")
-restaurantName.textContent = "Odin's Pizzaria"
+  const restaurantName = document.createElement("h1")
+  restaurantName.classList.add("restaurant-name")
+  restaurantName.textContent = "Odin's Pizzaria"
 
-header.appendChild(restaurantName)
-header.appendChild(navBar())
+  header.appendChild(restaurantName)
+  header.appendChild(navBar())
 
-return header
+  return header
 }
 
 // Creates the navbar with buttons, appends to header
 function navBar() {
-    const nav = document.createElement("nav")
+  const nav = document.createElement("nav")
 
-    const homeBtn = document.createElement("button")
-    homeBtn.classList.add("nav-btn")
-    homeBtn.textContent = "Home"
-    homeBtn.addEventListener("click", (e) => {
-        if (e.target.classList.contains("active")) return;
-        setActiveButton(homeBtn)
-        loadHome()
-    });
+  const homeBtn = document.createElement("button")
+  homeBtn.classList.add("nav-btn")
+  homeBtn.textContent = "Home"
+  homeBtn.addEventListener("click", (e) => {
+    console.log("Clicked Home")
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(homeBtn)
+    loadHome()
+  });
 
-    const menuBtn = document.createElement("button")
-    menuBtn.classList.add("nav-btn")
-    menuBtn.textContent = "Menu"
-    menuBtn.addEventListener("click", (e) => {
-        if (e.target.classList.contains("active")) return;
-        setActiveButton(menuBtn)
-        loadMenu()
-    });
+  const menuBtn = document.createElement("button")
+  menuBtn.classList.add("nav-btn")
+  menuBtn.textContent = "Menu"
+  menuBtn.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(menuBtn)
+    loadMenu()
+  });
 
-    const contactBtn = document.createElement("button")
-    contactBtn.classList.add("nav-btn")
-    contactBtn.textContent = "Contact"
-    contactBtn.addEventListener("click", (e) => {
-        if (e.target.classList.contains("active")) return;
-        setActiveButton(contactBtn)
-        loadContact()
-    });
-    
-    nav.appendChild(homeBtn);
-    nav.appendChild(menuBtn);
-    nav.appendChild(contactBtn);
-    
-    return nav;
+  const contactBtn = document.createElement("button")
+  contactBtn.classList.add("nav-btn")
+  contactBtn.textContent = "Contact"
+  contactBtn.addEventListener("click", (e) => {
+    console.log("Clicked")
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(contactBtn)
+    loadContact()
+  });
+
+  nav.appendChild(homeBtn);
+  nav.appendChild(menuBtn);
+  nav.appendChild(contactBtn);
+
+  return nav;
 }
 
 function setActiveButton(button) {
-    const buttons = document.querySelectorAll(".button-nav");
-  
-    buttons.forEach((button) => {
-      if (button !== this) {
-        button.classList.remove("active");
-      }
-    });
-  
-    button.classList.add("active");
-  }
-  
-  function createMain() {
-    const main = document.createElement("main");
-    main.classList.add("main");
-    main.setAttribute("id", "main");
-    return main;
-  }
+  const buttons = document.querySelectorAll(".button-nav");
 
-  // gets id="content"
-  function renderPage() {
+  buttons.forEach((button) => {
+    if (button !== this) {
+      button.classList.remove("active");
+    }
+  });
+
+  button.classList.add("active");
+}
+
+function createMain() {
+  const main = document.createElement("main");
+  main.classList.add("main");
+  main.setAttribute("id", "main");
+  return main;
+}
+
+// gets id="content"
+function renderPage() {
   const content = document.getElementById("content")
 
   content.appendChild(Header());
   content.appendChild(createMain())
   setActiveButton(document.querySelector(".nav-btn"))
   loadHome()
-  }
-  renderPage();
+}
+renderPage();
